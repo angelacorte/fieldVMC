8fc062e05c8f54a50178c0a9eead709761597c8e
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-05-05 15:41:59 +0200

    fix: upper case in repo name

--- field-vmc-basic-example.sh
+++ field-vmc-basic-example.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 DESTINATION="$HOME/Downloads/field-vmc-basic-example-$(date --utc "+%F-%H.%M.%S")"
-git clone https://github.com/angelacorte/fieldvmc.git "$DESTINATION"
+git clone https://github.com/angelacorte/fieldVMC.git "$DESTINATION"
 cd "$DESTINATION"
 ./gradlew runSelfConstructionGraphic
