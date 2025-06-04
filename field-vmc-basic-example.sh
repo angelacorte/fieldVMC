5ceba96ca7c67c44a8ca10788a3b7a95cda15fce
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-06-04 15:52:53 +0200

    fix: fast-run script

--- field-vmc-basic-example.sh
+++ field-vmc-basic-example.sh
@@ -2,4 +2,4 @@
 DESTINATION="$HOME/Downloads/field-vmc-basic-example-$(date --utc "+%F-%H.%M.%S")"
 git clone https://github.com/angelacorte/fieldVMC.git "$DESTINATION"
 cd "$DESTINATION"
-./gradlew runSelfConstructionGraphic
+./gradlew runSelfConstructionFieldVMCGraphic
